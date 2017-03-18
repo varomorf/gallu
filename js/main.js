4d0ed2373ba21997b8c41a436e84e740609c77dd
Álvaro Fernández González <varomorf@gmail.com>
2017-03-18 16:56:44 +0100

    Try adding a service worker.

---
+++ js/main.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Alvaro on 18/03/2017.
+ */
+
+// register the service worker if available
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('./sw.js').then(function(reg) {
+    console.log('Successfully registered service worker', reg);
+  }).catch(function(err) {
+    console.warn('Error whilst registering service worker', err);
+  });
+}
