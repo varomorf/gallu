63abed98fcbbdf454ace478a0997962fdcce6ce3
Álvaro Fernández González <varomorf@gmail.com>
2017-03-19 13:32:27 +0100

    Change ObjectivesList component to class and make it work with state.

--- js/ObjectivesList.js
+++ js/ObjectivesList.js
@@ -2,21 +2,27 @@
  * Created by Alvaro on 19/03/2017.
  */
 
-var ObjectivesList = React.createClass({
-  getInitialState: function () {
-    return { objectives: [] };
-  },
-  render: function () {
+class ObjectivesList extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      objectives: ['Objective one', 'Objective two', 'Objective three', 'Objective four']
+    };
+  }
+  getObjectives() {
+    return this.state.objectives.map((objective) =>
+      <li className="objectivesList-objectiveEntry">{objective}</li>
+    );
+  }
+  render() {
     return (
       <ul className="objectivesList">
-        <li className="objectivesList-objectiveEntry">Objective one</li>
-        <li className="objectivesList-objectiveEntry">Objective two</li>
-        <li className="objectivesList-objectiveEntry">Objective three</li>
-        <li className="objectivesList-objectiveEntry">Objective four</li>
+        {this.getObjectives()}
       </ul>
     );
   }
-});
+}
+
 ReactDOM.render(
 <ObjectivesList />,
   document.getElementById('objectivesContainer')
